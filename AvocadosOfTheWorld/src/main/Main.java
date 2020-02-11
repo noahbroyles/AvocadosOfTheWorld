d0a5542e5ae61438bc6bed2cbb60e3aaefdc7c44
broylend <broylend@MCD210-PC-02.uccc.uc.edu>
2020-02-11 11:51:02 -0500

    Added the right JRE

--- AvocadosOfTheWorld/src/main/Main.java
+++ AvocadosOfTheWorld/src/main/Main.java
@@ -5,16 +5,14 @@
  */
 package main;
 
+import fruit.Avocado;
 import fruit.Fruit;
 
 public class Main {
 
 	public static void main(String[] args) {
 		 
-		Fruit avocajo = new Fruit();
-		avocajo.setCost((float) 0.65);
-		avocajo.setWeight(1);
 		
-		System.out.println(avocajo);
+		Avocado avocajoe = new Avocado();
 	}
 }
