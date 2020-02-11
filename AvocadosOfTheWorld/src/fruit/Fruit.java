1ade038d90f0cdc6605769b38c0255b31578bae4
broylend <broylend@MCD210-PC-02.uccc.uc.edu>
2020-02-11 11:41:26 -0500

    Screwed around

--- AvocadosOfTheWorld/src/fruit/Fruit.java
+++ AvocadosOfTheWorld/src/fruit/Fruit.java
@@ -12,6 +12,45 @@ package fruit;
  */
 public abstract class Fruit {
 
+	private int weight;
+	private float cost;
 	
+	
+	
+	public Fruit () {}
+	
+	public Fruit(int weight, float cost) {
+		setWeight(weight);
+		setCost(cost);
+	}
+	
+	
+	
+	
+	// Getters and setters
+	/**
+	 * @return the weight
+	 */
+	public int getWeight() {
+		return weight;
+	}
+	/**
+	 * @param weight the weight to set
+	 */
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+	/**
+	 * @return the cost
+	 */
+	public float getCost() {
+		return cost;
+	}
+	/**
+	 * @param cost the cost to set
+	 */
+	public void setCost(float cost) {
+		this.cost = cost;
+	}
 	
 }
